ed0a805842fcfcd974d313d65a899807e0e926f5
Braden Soller <braden.soller18@gmail.com>
2024-12-19 12:33:24 -0600

    added axios get route to receive math from server

--- server/public/src/index.ts
+++ server/public/src/index.ts
@@ -1,2 +1,17 @@
 console.log('hello')
+import axios from "axios";
 
+let result = 0;
+let operator = '';
+//gets history AKA culculations results // we set the newCalculation object = to the response body
+function getHistory() {
+    axios({
+        url: '/calculations',
+        method: 'GET'
+      }).then((response:any) => {
+        let calculations = response.data 
+       // renderCalulation(calculations)
+        console.log('this data is being rendered', calculations);
+                         
+    });
+}
